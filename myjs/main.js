a8b970e63084ecf4ce12e615dbb52d425c8900bc
mobina-ghafari <135110634+mobina-ghafari@users.noreply.github.com>
2023-09-19 03:57:35 -0700

    main-js-finish

--- myjs/main.js
+++ myjs/main.js
@@ -23,3 +23,31 @@ $(window).scroll(function(){
         $('.honors-img').removeClass('fromLeft')
     }
 })
+
+$('.gallery-item').click(function(){
+    let value = $(this).attr('data-filter');
+    $(this).addClass('active-item').siblings().removeClass('active-item')
+
+    if(value === 'all'){
+        $('.filter').show(300)
+    }else{
+        $('.filter').not('.' + value).hide(300);
+        $('.filter').filter('.' + value).show(300);
+    }
+})
+
+$(window).scroll(function(){
+    let position = $(this).scrollTop();
+    console.log(position)
+    if(position >= 4400 ){
+        $('.card1').addClass('moveFromRight')
+        $('.card2').addClass('moveFromBottom')
+        $('.card3').addClass('moveFromLeft')
+
+    }else{
+        $('.card1').removeClass('moveFromRight')
+        $('.card2').removeClass('moveFromBottom')
+        $('.card3').removeClass('moveFromLeft')
+
+    }
+})
